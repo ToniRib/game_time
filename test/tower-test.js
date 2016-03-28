cffea427bd728d158ea1905d3022da82c9929709
Jordan Lawler <jordan.lawler@colorado.edu>
2016-03-28 17:02:46 -0600

    Update simple tower tests to reference class number instead of integer

--- test/tower-test.js
+++ test/tower-test.js
@@ -1,6 +1,25 @@
 var assert = require('chai').assert;
-const tower = require('../lib/tower.js');
+const SimpleTower = require('../lib/simple-tower.js');
 
 it('tower is a function', function (){
-  assert.typeOf(tower, 'function');
+  assert.typeOf(SimpleTower, 'function');
+});
+
+describe('tower attributes', function (){
+
+  it('tower has a damage amount', function (){
+    let tower = new SimpleTower({x: 1, y: 1});
+    assert.typeOf(tower.damage, 'number');
+  });
+
+  it('tower has a range', function (){
+    let tower = new SimpleTower({x: 1, y: 1});
+    assert.typeOf(tower.range, 'number');
+  });
+
+  it('tower has a fire-rate', function (){
+    let tower = new SimpleTower({x: 1, y: 1});
+    assert.typeOf(tower.fireRate, 'number');
+  });
+
 });
