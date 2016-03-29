629c0395dcc5c7dd6272481221a8455ebd5f8742
Jordan Lawler <jordan.lawler@colorado.edu>
2016-03-29 17:56:18 -0600

    added comments to board testing

--- test/board-test.js
+++ test/board-test.js
@@ -7,11 +7,14 @@ describe('can build a board', function(){
     let board = new Board({});
     assert.typeOf(board.tiles, 'array');
     assert.equal(board.tiles.length, 160);
+    // Tile 0 is the beggining of the first row, 1 is second tile of the first row
     assert.equal(board.tiles[0].x, 0);
     assert.equal(board.tiles[1].x, 50);
+    // Tile 15 is the end of the first row, 16 is the beginning of the second row
     assert.equal(board.tiles[15].x, 750);
     assert.equal(board.tiles[16].x, 0);
     assert.equal(board.tiles[0].y, 0);
+    // Tile 16 is the beginning of the second row, 159 is the last tile of the board
     assert.equal(board.tiles[16].y, 50);
     assert.equal(board.tiles[159].y, 450);
   });
