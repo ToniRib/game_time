def91071c37dae88cfac1a9c1f850102eca0be01
ToniRib <tonimarierib@gmail.com>
2016-03-29 13:18:54 -0600

    Start adding tests and functionality for the Tile class

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+require('./tile-test.js');
 require('./simple-tower-test.js');
 require('./simple-enemy-test.js');
 require('./game-test.js');
--- test/tile-test.js
+++ test/tile-test.js
@@ -4,3 +4,45 @@ const Tile = require('../lib/tile.js');
 it('game is a function', function (){
   assert.typeOf(Tile, 'function');
 });
+
+describe('tile attributes', function() {
+  it('has a x coord', function() {
+    let tile = new Tile({ isBuildSite: true }, { x: 1, y: 2 });
+    assert.typeOf(tile.x, 'number');
+    assert.equal(tile.x, 1);
+  });
+
+  it('has a y coord', function() {
+    let tile = new Tile({ isBuildSite: true }, { x: 1, y: 2 });
+    assert.typeOf(tile.y, 'number');
+    assert.equal(tile.y, 2);
+  });
+
+  it('is vacant by default', function() {
+    let tile = new Tile({ isBuildSite: true }, { x: 1, y: 2 });
+    assert(tile.vacant);
+  });
+
+  it('has a width of 50 by default', function() {
+    let tile = new Tile({ isBuildSite: true }, { x: 1, y: 2 });
+    assert.equal(tile.width, 50);
+  });
+
+  it('has a height of 50 by default', function() {
+    let tile = new Tile({ isBuildSite: true }, { x: 1, y: 2 });
+    assert.equal(tile.height, 50);
+  });
+
+  it('is a build site', function() {
+    let tile = new Tile({ isBuildSite: true }, { x: 1, y: 2 });
+    assert(tile.buildSite);
+  });
+});
+
+describe('adds a tower to site', function() {
+  it('can add a tower if vacant and a build site', function() {
+    let tile = new Tile({ isBuildSite: true }, { x: 1, y: 2 });
+    tile.addTower('simple');
+    assert.typeOf(tile.tower, 'object');
+  });
+});
