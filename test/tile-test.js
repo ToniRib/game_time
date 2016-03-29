7d72ca72b6b0d746a9d41cce3d07bc50096212cb
ToniRib <tonimarierib@gmail.com>
2016-03-29 14:36:37 -0600

    Commit prior to restructuring Tile class to break apart into separate subclasses

--- test/tile-test.js
+++ test/tile-test.js
@@ -45,4 +45,28 @@ describe('adds a tower to site', function() {
     tile.addTower('simple');
     assert.typeOf(tile.tower, 'object');
   });
+
+  it('cant add a tower to a filled build site', function() {
+    let tile = new Tile({ isBuildSite: true }, { x: 1, y: 2 });
+    tile.vacant = false;
+    tile.tower = 'fakeTower';
+    tile.addTower('simple');
+    assert.typeOf(tile.tower, 'string');
+  });
+
+  it('cant add a tower if tile is not build site', function() {
+    let tile = new Tile({ isBuildSite: false }, { x: 1, y: 2 });
+    tile.vacant = true;
+    tile.tower = 'vacant';
+    tile.addTower('simple');
+    assert.typeOf(tile.tower, 'string');
+    assert.equal(tile.tower, 'vacant');
+  });
+
+  it('has correct origin point', function() {
+    let tile = new Tile({ isBuildSite: true }, { x: 50, y: 50 });
+    tile.addTower('simple');
+    assert.equal(tile.tower.y, 75);
+    assert.equal(tile.tower.x, 75);
+  });
 });
