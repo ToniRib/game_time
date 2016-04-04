0a30fef1f380b3585ccda27bac3f3816f1c86922
ToniRib <tonimarierib@gmail.com>
2016-04-04 15:15:54 -0600

    Set up MonsterEnemy which is super strong but super slow

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ require('./sprite-test.js');
 require('./image-loader-test.js');
 require('./game-test.js');
 require('./quick-enemy-test.js');
+require('./monster-enemy-test.js');
 require('./flash-tower-test.js');
 require('./simple-enemy-test.js');
 require('./board-test.js');
